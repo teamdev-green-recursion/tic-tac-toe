6a864f94f9a06b4e037bbc86c2c8d0c0cef59976
kotaroito <agkito34@gmail.com>
2023-02-26 20:56:27 -0800

    added functionality to restart the game when the button is pressed

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const board = ['', '', '', '', '', '', '', '', ''];
+let board = ['', '', '', '', '', '', '', '', ''];
 let player = 'X';
 
 const btns = document.querySelectorAll('.square-btn');
+const restart_btn = document.getElementById("restart");
 
 // プレイヤーがボタンをクリックした場合呼び出される
 function handleButtonClick(button, index) {
@@ -12,8 +13,21 @@ function handleButtonClick(button, index) {
   }
 }
 
+// プレイヤーがRestartボタンを押した時に呼び出される
+// 全てのボタンのテキスト、ボードを表す配列　board、ターンを示すplayerを初期化
+function handleRestart() {
+  btns.forEach(function(ele) {
+    ele.textContent = "";
+  });
+
+  board = ['', '', '', '', '', '', '', '', ''];
+  player = 'X';
+}
+
 btns.forEach((button, index) => {
   button.addEventListener('click', () => {
     handleButtonClick(button, index);
   });
 });
+
+restart_btn.addEventListener('click', handleRestart);
