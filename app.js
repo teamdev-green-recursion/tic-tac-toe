57d06d315d88caade02e1c1214b8d23df2bb19bd
Masaki Fukumoto <fm8624@gmail.com>
2023-02-27 14:28:56 +0900

    Added user switching process

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ let board = ['', '', '', '', '', '', '', '', ''];
 let player = 'X';
 
 const btns = document.querySelectorAll('.square-btn');
-const restart_btn = document.getElementById("restart");
+const turn = document.querySelector('#turn');
+const restart_btn = document.getElementById('restart');
 
 // プレイヤーがボタンをクリックした場合呼び出される
 function handleButtonClick(button, index) {
@@ -10,14 +11,17 @@ function handleButtonClick(button, index) {
     board[index] = player;
     button.textContent = player;
     player = player === 'X' ? 'O' : 'X';
+
+    // プレイヤーのターン表示切り替え
+    turn.textContent = `${player}'s turn`;
   }
 }
 
 // プレイヤーがRestartボタンを押した時に呼び出される
-// 全てのボタンのテキスト、ボードを表す配列　board、ターンを示すplayerを初期化
+// 全てのボタンのテキスト、ボードを表す配列 board、ターンを示すplayerを初期化
 function handleRestart() {
-  btns.forEach(function(ele) {
-    ele.textContent = "";
+  btns.forEach(function (ele) {
+    ele.textContent = '';
   });
 
   board = ['', '', '', '', '', '', '', '', ''];
